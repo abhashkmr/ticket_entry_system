a3ebbf43358617420d58f3eaafddd2ac9ca38018
Abhash <abhash.kumar@zopsmart.com>
2024-01-18 23:16:21 +0530

    added cors for better local development

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 // backend/server.js
 import Koa from 'koa';
+import cors from '@koa/cors'
 import Router from '@koa/router';
 import bodyParser from 'koa-bodyparser';
 import { connectToDB } from './db.js';
@@ -12,6 +13,7 @@ const app = new Koa();
 const router = new Router()
 
 
+app.use(cors())
 app.use(bodyParser())
 app.use(router.routes())
 
